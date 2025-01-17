44fd71b6b92bd2932ff2baabe8c3f92bf62b21d9
daniel.baumann <baumann-dan@outlook.com>
2025-01-17 18:08:06 +0100

    add portaudio dependency

--- src/app.cpp
+++ src/app.cpp
@@ -1,3 +1,8 @@
-//
-// Created by daniel on 12.01.25.
-//
+#include <cstdlib>
+#include <iostream>
+#include <portaudio.h>
+
+int main() {
+    std::cout << "Hello World";
+    return EXIT_SUCCESS;
+}
