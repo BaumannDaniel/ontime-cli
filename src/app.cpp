697e8d7dab927ef40ac8105b4f5ad56dcaf0d73c
daniel.baumann <baumann-dan@outlook.com>
2025-01-19 17:22:37 +0100

    include miniaudio

--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,25 @@
 #include <cstdlib>
 #include <iostream>
 
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
+
+extern "C" {
 int main() {
-    std::cout << "Hello World";
+    ma_result result;
+    ma_engine engine;
+
+    result = ma_engine_init(nullptr, &engine);
+    if (result != MA_SUCCESS) {
+        return -1;
+    }
+
+    ma_engine_play_sound(&engine, "/home/daniel/Downloads/M1F1-Alaw-AFsp.wav", nullptr);
+
+    printf("Press Enter to quit...");
+    getchar();
+
+    ma_engine_uninit(&engine);
     return EXIT_SUCCESS;
+}
 }
