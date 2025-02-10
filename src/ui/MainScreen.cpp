b356be341a60a301e59ff94d2e7d231ab064b68e
daniel.baumann <baumann-dan@outlook.com>
2025-02-10 20:57:58 +0100

    refactor MainScreen

--- src/ui/MainScreen.cpp
+++ src/ui/MainScreen.cpp
@@ -3,6 +3,51 @@
 tone::ui::MainScreen::MainScreen(
     const MainInputProcessor &main_input_processor
 ): main_input_processor(main_input_processor) {
+    main_input_option = {
+        .multiline = false,
+        .on_enter = [&] {
+            main_input_processor.process(main_input_value);
+            main_input_value.clear();
+        }
+    };
+    main_input = Input(&main_input_value, "Type your command here!", main_input_option);
+    tab_toggle = ftxui::Toggle(&tab_values, &tab_selected);
+    editor_container = ftxui::Container::Vertical({});
+    devices_container = ftxui::Container::Vertical({});
+    settings_container = ftxui::Container::Vertical({});
+    tab_container = ftxui::Container::Tab(
+        {
+            Renderer(
+                editor_container,
+                [&] { return ftxui::text("1"); }
+            ),
+            Renderer(
+                devices_container,
+                [&] { return ftxui::text("2"); }
+            ),
+            Renderer(
+                settings_container,
+                [&] { return ftxui::text("3"); }
+            )
+        },
+        &tab_selected
+    );
+    root_container = ftxui::Container::Vertical({
+        main_input,
+        tab_toggle,
+        tab_container
+    });
+    renderer = Renderer(
+        root_container,
+        [&] {
+            return vbox(
+                main_input->Render(),
+                ftxui::separator(),
+                tab_toggle->Render(),
+                ftxui::separator(),
+                tab_container->Render()
+            );
+        });
 }
 
 ftxui::Component tone::ui::MainScreen::render() {
--- src/ui/MainScreen.h
+++ src/ui/MainScreen.h
@@ -7,70 +7,38 @@
 #include "Screen.h"
 
 namespace tone::ui {
+    struct MainScreenTabs {
+        constexpr std::string EDITOR = "Editor";
+        constexpr std::string DEVICES = "Devices";
+        constexpr std::string SETTINGS = "Settings";
+    };
+
     class MainScreen : Screen {
         MainInputProcessor main_input_processor;
         std::string main_input_value;
-        ftxui::InputOption main_input_option = {
-            .multiline = false,
-            .on_enter = [&] {
-                main_input_processor.process(main_input_value);
-                main_input_value.clear();
-            }
-        };
-        ftxui::Component main_input = Input(&main_input_value, "Type your command here!", main_input_option);
+        ftxui::InputOption main_input_option;
+        ftxui::Component main_input;
         int tab_selected = 0;
-        std::vector<std::string> tab_values{
-            "Editor",
-            "Devices",
-            "Settings",
+        const std::vector<std::string> tab_values{
+            MainScreenTabs::EDITOR,
+            MainScreenTabs::DEVICES,
+            MainScreenTabs::SETTINGS,
         };
-        ftxui::Component tab_toggle = ftxui::Toggle(&tab_values, &tab_selected);
-        ftxui::Component editor_container = ftxui::Container::Vertical({});
-        ftxui::Component devices_container = ftxui::Container::Vertical({});
-        ftxui::Component settings_container = ftxui::Container::Vertical({});
-        ftxui::Component tab_container = ftxui::Container::Tab(
-            {
-                Renderer(
-                    editor_container,
-                    [&] { return ftxui::text("1"); }
-                ),
-                Renderer(
-                    devices_container,
-                    [&] { return ftxui::text("2"); }
-                ),
-                Renderer(
-                    settings_container,
-                    [&] { return ftxui::text("3"); }
-                )
-            },
-            &tab_selected
-        );
-
-        // The component tree:
-        ftxui::Component root_container = ftxui::Container::Vertical({
-            main_input,
-            tab_toggle,
-            tab_container
-        });
-
-        // Tweak how the component tree is rendered:
-        ftxui::Component renderer = Renderer(
-            root_container,
-            [&] {
-                return vbox(
-                    main_input->Render(),
-                    ftxui::separator(),
-                    tab_toggle->Render(),
-                    ftxui::separator(),
-                    tab_container->Render()
-                );
-            });
+        ftxui::Component tab_toggle;
+        ftxui::Component editor_container;
+        ftxui::Component devices_container;
+        ftxui::Component settings_container;
+        ftxui::Component tab_container;
+        ftxui::Component root_container;
+        ftxui::Component renderer;
 
     public:
         explicit MainScreen(const MainInputProcessor &main_input_processor);
 
         void start() override;
+
         void stop() override;
+
         ftxui::Component render() override;
     };
 }
