295f565e09abd8a83794b9abaf0d66e3c56ce396
daniel.baumann <baumann-dan@outlook.com>
2025-03-05 21:46:48 +0100

    add PlayerInfo struct

--- src/Player.h
+++ src/Player.h
@@ -1,11 +1,24 @@
 #pragma once
+
+#include <string>
+#include <boost/uuid/uuid.hpp>
+
 #include "DeviceState.h"
 
-class Player {
-public:
-    DeviceState state = INIT;
-    virtual ~Player() = default;
-    virtual void play() = 0;
-    virtual void pause() = 0;
-    virtual void finish() = 0;
-};
+namespace tone {
+    class Player {
+    public:
+        DeviceState state = INIT;
+        virtual ~Player() = default;
+        virtual void play() = 0;
+        virtual void pause() = 0;
+        virtual void finish() = 0;
+    };
+
+    struct PlayerInfo {
+        boost::uuids::uuid id;
+        std::string file_name;
+        uint64_t length;
+        uint64_t position;
+    };
+}
