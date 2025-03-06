3f1a66eb2bbe6981cd34bee1901963ac906eefb1
daniel.baumann <baumann-dan@outlook.com>
2025-03-06 21:29:18 +0100

    add PlayerInfo class

--- src/Player.cpp
+++ src/Player.cpp
@@ -3,3 +3,42 @@
 tone::DeviceState tone::Player::getDeviceState() {
     return this->state;
 }
+
+tone::PlayerInfo::PlayerInfo(
+    boost::uuids::uuid player_id,
+    std::string file_name,
+    uint64_t file_length,
+    uint64_t file_position
+) : id(player_id),
+    file_name(file_name),
+    file_length(file_length),
+    file_position(file_position) {
+}
+
+void tone::PlayerInfo::set_file_name(std::string file_name) {
+    this->file_name = file_name;
+}
+
+void tone::PlayerInfo::set_file_length(uint64_t file_length) {
+    this->file_length = file_length;
+}
+
+void tone::PlayerInfo::set_file_position(uint64_t file_position) {
+    this->file_position = file_position;
+}
+
+boost::uuids::uuid tone::PlayerInfo::get_id() {
+    return this->id;
+}
+
+std::string tone::PlayerInfo::get_file_name() {
+    return this->file_name;
+}
+
+uint64_t tone::PlayerInfo::get_file_length() {
+    return this->file_length;
+}
+
+uint64_t tone::PlayerInfo::get_file_position() {
+    return this->file_position;
+}
--- src/Player.h
+++ src/Player.h
@@ -6,21 +6,54 @@
 #include "DeviceState.h"
 
 namespace tone {
+    class PlayerInfo;
+
     class Player {
     protected:
         DeviceState state = INIT;
+
     public:
         virtual DeviceState getDeviceState();
+
         virtual ~Player() = default;
+
         virtual void play() = 0;
+
         virtual void pause() = 0;
+
         virtual void finish() = 0;
+
+        virtual PlayerInfo get_player_info() = 0;
     };
 
-    struct PlayerInfo {
+    class PlayerInfo {
         boost::uuids::uuid id;
         std::string file_name;
-        uint64_t length;
-        uint64_t position;
+        uint64_t file_length;
+        uint64_t file_position;
+
+        void set_file_name(std::string file_name);
+
+        void set_file_length(uint64_t file_length);
+
+        void set_file_position(uint64_t file_position);
+
+    public:
+        PlayerInfo(
+            boost::uuids::uuid player_id,
+            std::string file_name,
+            uint64_t file_length,
+            uint64_t file_position
+        );
+
+        boost::uuids::uuid get_id();
+
+        std::string get_file_name();
+
+        uint64_t get_file_length();
+
+        uint64_t get_file_position();
+
+        friend class Player;
     };
 }
