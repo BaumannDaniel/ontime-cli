021c663f322e59831486137622dff86efc9bdffd
daniel.baumann <baumann-dan@outlook.com>
2025-03-31 21:40:45 +0200

    add FormattingTest

--- test/unit/ui/InputTest.cpp
+++ test/unit/ui/InputTest.cpp
@@ -2,25 +2,25 @@
 
 #include "input.h"
 
-TEST(trimWhitespace__removes_leading_whitespace, BasicAsserstions) {
+TEST(trimWhitespace, removes_leading_whitespace) {
     const std::string test_string = " test string";
     const auto trimmed_string = tone::ui::trim_whitespace(test_string);
     ASSERT_EQ("test string", trimmed_string);
 }
 
-TEST(trimWhitespace__removes_trailing_whitespace, BasicAsserstions) {
+TEST(trimWhitespace, removes_trailing_whitespace) {
     const std::string test_string = "test string ";
     const auto trimmed_string = tone::ui::trim_whitespace(test_string);
     ASSERT_EQ("test string", trimmed_string);
 }
 
-TEST(trimWhitespace__removes_double_whitespace, BasicAsserstions) {
+TEST(trimWhitespace, removes_double_whitespace) {
     const std::string test_string = "  test  string  ";
     const auto trimmed_string = tone::ui::trim_whitespace(test_string);
     ASSERT_EQ("test string", trimmed_string);
 }
 
-TEST(parseInput__splits_input_string, BasicAsserstions) {
+TEST(parseInput, splits_input_string) {
     const std::string test_string = "  test  string  ";
     const auto parsed_input = tone::ui::parse_input(test_string);
     ASSERT_EQ(2, parsed_input.size());
@@ -28,7 +28,7 @@ TEST(parseInput__splits_input_string, BasicAsserstions) {
     ASSERT_EQ(parsed_input[1], "string");
 }
 
-TEST(parseInput__handles_empty_string, BasicAsserstions) {
+TEST(parseInput, handles_empty_string) {
     const std::string test_string = "  ";
     const auto parsed_input = tone::ui::parse_input(test_string);
     ASSERT_EQ(0, parsed_input.size());
