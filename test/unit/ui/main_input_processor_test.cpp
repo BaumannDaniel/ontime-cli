3a2c72c09d5fa21df52400a6ee5e181672d7cd51
daniel.baumann <baumann-dan@outlook.com>
2025-04-10 21:29:11 +0200

    add unit tests for MainInputProcessor

--- test/unit/ui/main_input_processor_test.cpp
+++ test/unit/ui/main_input_processor_test.cpp
@@ -4,6 +4,7 @@
 #include "config.h" // NOLINT
 #include "main_input_processor.h"
 
+#include <boost/uuid/random_generator.hpp>
 
 #include "device_id_manager.h"
 #include "player.h"
@@ -13,6 +14,10 @@
 namespace tone::test {
     class MainInputProcessorTest : public testing::Test {
     protected:
+        const std::string test_file_1 = "/testPath/test1.mp3";
+        const std::string player_1_ui_id = "a";
+        const boost::uuids::uuid player_1_device_id = boost::uuids::random_generator()();
+
         std::shared_ptr<ui::DeviceIdManager> device_id_manager;
         std::shared_ptr<MockLogger> logger;
         std::shared_ptr<MockDeviceFacade> device_facade;
@@ -28,10 +33,55 @@ namespace tone::test {
         }
     };
 
-    TEST_F(MainInputProcessorTest, process000addsPlayerToDeviceFacade) {
-        std::string input = "add player /testPath/test.mp3";
-        auto facade = device_facade.get();
-        EXPECT_CALL(*facade, addPlayer(testing::_)).Times(1);
+    TEST_F(MainInputProcessorTest, process$addsPlayerToDeviceFacade) {
+        const std::string input = std::format("add player {}", test_file_1);
+        const auto mock_device_facade = device_facade.get();
+        EXPECT_CALL(
+            *mock_device_facade,
+            addPlayer(testing::Truly([&](const std::shared_ptr<Player>& player) {
+                auto correct_file = player->getPlayerInfo()->getFileName() == test_file_1;
+                auto correct_state = player->getDeviceState() == DeviceState::UN_INIT;
+                return correct_file && correct_state;
+                }))
+        ).Times(1);
+        main_input_processor.process(input);
+    }
+
+    TEST_F(MainInputProcessorTest, process$startsPlayer) {
+        device_id_manager->addIdMapping(player_1_ui_id, player_1_device_id, ui::PLAYER);
+        const std::string input = std::format("start {}", player_1_ui_id);
+        const auto mock_device_facade = device_facade.get();
+        EXPECT_CALL(
+            *mock_device_facade,
+            startPlayer(player_1_device_id)
+        );
+        main_input_processor.process(input);
+    }
+
+    TEST_F(MainInputProcessorTest, process$playsMp3File) {
+        const std::string input = std::format("play {}", test_file_1);
+        const auto mock_device_facade = device_facade.get();
+        boost::uuids::uuid player_device_id;
+        testing::InSequence seq;
+        EXPECT_CALL(
+            *mock_device_facade,
+            addPlayer(testing::Truly([&](const std::shared_ptr<Player>& player) {
+                    player_device_id = player->getPlayerInfo()->getId();
+                    auto correct_file = player->getPlayerInfo()->getFileName() == test_file_1;
+                    auto correct_state = player->getDeviceState() == DeviceState::UN_INIT;
+                    return correct_file && correct_state;
+                    }
+                )
+            )
+        );
+        EXPECT_CALL(
+            *mock_device_facade,
+            startPlayer(testing::Truly([&](const boost::uuids::uuid &id) {
+                    return id == player_device_id;
+                    }
+                )
+            )
+        );
         main_input_processor.process(input);
     }
 }
