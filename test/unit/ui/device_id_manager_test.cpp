702877e0caf03fa9f89bf372f63dc2d89e10d516
daniel.baumann <baumann-dan@outlook.com>
2025-04-12 07:02:13 +0200

    add unit tests for DeviceIdManager

--- test/unit/ui/device_id_manager_test.cpp
+++ test/unit/ui/device_id_manager_test.cpp
@@ -10,7 +10,7 @@ namespace tone::test {
     };
 
     TEST_F(DeviceIdManagerTest, addIdMapping$addsMapping) {
-        std::string ui_id = device_id_manager.getAvailableUiId();
+        auto ui_id = device_id_manager.getAvailableUiId().value();
         auto device_id = boost::uuids::random_generator()();
         auto device_type = ui::DeviceType::PLAYER;
         device_id_manager.addIdMapping(ui_id, device_id, device_type);
@@ -23,15 +23,43 @@ namespace tone::test {
     }
 
     TEST_F(DeviceIdManagerTest, removeIdMappingByDeviceId$removesMapping) {
-        std::string ui_id = device_id_manager.getAvailableUiId();
+        auto ui_id = device_id_manager.getAvailableUiId().value();
+        auto device_id = boost::uuids::random_generator()();
+        auto device_type = ui::DeviceType::PLAYER;
+        device_id_manager.addIdMapping(ui_id, device_id, device_type);
+        device_id_manager.removeIdMappingByDeviceId(device_id);
+        auto device_id_from_ui_id = device_id_manager.getDeviceId(ui_id);
+        auto ui_id_from_device_id = device_id_manager.getUiId(device_id);
+        auto device_type_from_ui_id = device_id_manager.getDeviceTypeByUiId(ui_id);
+        EXPECT_FALSE(device_id_from_ui_id.has_value());
+        EXPECT_FALSE(ui_id_from_device_id.has_value());
+        EXPECT_FALSE(device_type_from_ui_id.has_value());
+    }
+
+    TEST_F(DeviceIdManagerTest, getDeviceId$returnsDeviceId) {
+        auto ui_id = device_id_manager.getAvailableUiId().value();
         auto device_id = boost::uuids::random_generator()();
         auto device_type = ui::DeviceType::PLAYER;
         device_id_manager.addIdMapping(ui_id, device_id, device_type);
         auto device_id_from_ui_id = device_id_manager.getDeviceId(ui_id);
-        auto ui_id_from_device_id = device_id_manager.getUiId(device_id);
-        auto device_type_from_ui_id = device_id_manager.getDeviceTypeByUiId(ui_id);
         EXPECT_EQ(device_id, device_id_from_ui_id);
+    }
+
+    TEST_F(DeviceIdManagerTest, getUiId$returnsUiId) {
+        auto ui_id = device_id_manager.getAvailableUiId().value();
+        auto device_id = boost::uuids::random_generator()();
+        auto device_type = ui::DeviceType::PLAYER;
+        device_id_manager.addIdMapping(ui_id, device_id, device_type);
+        auto ui_id_from_device_id = device_id_manager.getUiId(device_id);
         EXPECT_EQ(ui_id, ui_id_from_device_id);
+    }
+
+    TEST_F(DeviceIdManagerTest, getDeviceTypeByUiId$returnsDeviceType) {
+        auto ui_id = device_id_manager.getAvailableUiId().value();
+        auto device_id = boost::uuids::random_generator()();
+        auto device_type = ui::DeviceType::PLAYER;
+        device_id_manager.addIdMapping(ui_id, device_id, device_type);
+        auto device_type_from_ui_id = device_id_manager.getDeviceTypeByUiId(ui_id);
         EXPECT_EQ(device_type, device_type_from_ui_id);
     }
 }
