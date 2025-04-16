90cc7228e6719f0f9b34f494a10c2083c60da30f
daniel.baumann <baumann-dan@outlook.com>
2025-04-16 20:44:54 +0200

    adjust tests to IPlayer class

--- test/unit/util/player_mock.h
+++ test/unit/util/player_mock.h
@@ -5,17 +5,14 @@
 #include "player.h"
 
 namespace tone::test {
-    class MockPlayer : public Player {
+    class MockPlayer : public IPlayer {
     public:
-        MockPlayer(): Player("", std::make_shared<MockLogger>()) {
-        }
-
         MOCK_METHOD(void, init, (), (override));
         MOCK_METHOD(void, start, (), (override));
         MOCK_METHOD(void, stop, (), (override));
         MOCK_METHOD(void, unInit, (), (override));
         MOCK_METHOD(void, changeFile, (std::string file_name), (override));
         MOCK_METHOD(DeviceState, getDeviceState, (), (const, override));
-        MOCK_METHOD(std::shared_ptr<PlayerInfo>, getPlayerInfo, (), (override));
+        MOCK_METHOD(std::shared_ptr<IPlayerInfo>, getPlayerInfo, (), (const, override));
     };
 }
