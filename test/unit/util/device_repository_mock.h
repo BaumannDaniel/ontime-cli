c1c18d3fff7e8790375b429ca16a6eb0ff0a8983
daniel.baumann <baumann-dan@outlook.com>
2025-04-16 20:54:46 +0200

    adjust tests to IPlayer class

--- test/unit/util/device_repository_mock.h
+++ test/unit/util/device_repository_mock.h
@@ -6,11 +6,11 @@
 namespace tone::test {
     class MockDeviceRepository final : public IDeviceRepository {
     public:
-        MOCK_METHOD(void, addPlayer, (std::shared_ptr<Player> player), (override));
+        MOCK_METHOD(void, addPlayer, (std::shared_ptr<IPlayer> player), (override));
         MOCK_METHOD(void, removePlayer, (boost::uuids::uuid player_id), (override));
         MOCK_METHOD(void, startPlayer, (boost::uuids::uuid player_id), (const, override));
         MOCK_METHOD(void, stopPlayer, (boost::uuids::uuid player_id), (const, override));
         MOCK_METHOD(void, changePlayerFile, (boost::uuids::uuid player_id, std::string file_name), (const, override));
-        MOCK_METHOD(std::vector<std::shared_ptr<PlayerInfo> >, getPlayersInfo, (), (const, override));
+        MOCK_METHOD(std::vector<std::shared_ptr<IPlayerInfo> >, getPlayersInfo, (), (const, override));
     };
 }
