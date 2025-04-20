4fddac77b787c5e8d3232705db7d74ce661ebfa5
daniel.baumann <baumann-dan@outlook.com>
2025-04-20 12:58:11 +0200

    integrate recorder in media screen

--- src/ui/media_screen.cpp
+++ src/ui/media_screen.cpp
@@ -6,6 +6,7 @@
 
 #include "device_id_manager.h"
 #include "player_component.h"
+#include "recorder_component.h"
 
 
 namespace tone::ui {
@@ -13,7 +14,7 @@ namespace tone::ui {
         std::shared_ptr<ILogger> logger;
         std::shared_ptr<IDeviceRepository> device_facade;
         std::shared_ptr<DeviceIdManager> device_id_mapper;
-        std::map<boost::uuids::uuid, ftxui::Component> player_components;
+        std::map<boost::uuids::uuid, ftxui::Component> device_components;
 
     public:
         MediaScreenBase(
@@ -37,25 +38,35 @@ tone::ui::MediaScreenBase::MediaScreenBase(
 
 ftxui::Element tone::ui::MediaScreenBase::Render() {
     auto players_info = device_facade->getPlayersInfo();
-    std::set<boost::uuids::uuid> current_player_ids;
+    auto recorders_info = device_facade->getRecordersInfo();
+    std::set<boost::uuids::uuid> current_device_ids;
     for (const auto &player_info: players_info) {
         auto player_id = player_info->getId();
-        current_player_ids.insert(player_id);
-        if (player_components.contains(player_id)) continue;
+        current_device_ids.insert(player_id);
+        if (device_components.contains(player_id)) continue;
         auto player_ui_id = device_id_mapper->getAvailableUiId();
         auto player_component = createPlayerComponent(player_ui_id.value(), player_info);
-        player_components.insert({player_id, player_component});
+        device_components.insert({player_id, player_component});
         device_id_mapper->addIdMapping(player_ui_id.value(), player_id, PLAYER);
     }
-    for (const auto& player : player_components) {
-        if (!current_player_ids.contains(player.first)) {
-            player_components.erase(player.first);
-            device_id_mapper->removeIdMappingByDeviceId(player.first);
+    for (const auto &recorder_info: recorders_info) {
+        auto recorder_id = recorder_info->getId();
+        current_device_ids.insert(recorder_id);
+        if (device_components.contains(recorder_id)) continue;
+        auto recorder_ui_id = device_id_mapper->getAvailableUiId();
+        auto recorder_component = createRecorderComponent(recorder_ui_id.value(), recorder_info);
+        device_components.insert({recorder_id, recorder_component});
+        device_id_mapper->addIdMapping(recorder_ui_id.value(), recorder_id, RECORDER);
+    }
+    for (const auto& device : device_components) {
+        if (!current_device_ids.contains(device.first)) {
+            device_components.erase(device.first);
+            device_id_mapper->removeIdMappingByDeviceId(device.first);
         }
     }
     std::vector<ftxui::Element> player_elements;
-    for (const auto& player: player_components) {
-        player_elements.push_back(player.second->Render());
+    for (const auto& device: device_components) {
+        player_elements.push_back(device.second->Render());
     }
     return vbox(player_elements);
 }
