25de34c4fb9032da34d54e837ca649f2ad084a8f
daniel.baumann <baumann-dan@outlook.com>
2025-04-20 20:50:59 +0200

    add unit test for recorder integration in device repository

--- test/unit/util/recorder_mock.h
+++ test/unit/util/recorder_mock.h
@@ -1,17 +1,15 @@
 #pragma once
 #include <gmock/gmock-function-mocker.h>
 
-#include "player.h"
+#include "recorder_i.h"
 
 namespace tone::test {
-    class MockPlayer : public IPlayer {
+    class MockRecorder : public IRecorder {
     public:
         MOCK_METHOD(void, init, (), (override));
         MOCK_METHOD(void, start, (), (override));
         MOCK_METHOD(void, stop, (), (override));
         MOCK_METHOD(void, unInit, (), (override));
-        MOCK_METHOD(void, changeFile, (std::string file_name), (override));
-        MOCK_METHOD(DeviceState, getDeviceState, (), (const, override));
-        MOCK_METHOD(std::shared_ptr<IPlayerInfo>, getPlayerInfo, (), (const, override));
+        MOCK_METHOD(std::shared_ptr<IRecorderInfo>, getRecorderInfo, (), (const, override));
     };
 }
