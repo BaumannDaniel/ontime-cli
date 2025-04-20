0d5685c8c759a034d965555b10bb0eee9f1eede5
daniel.baumann <baumann-dan@outlook.com>
2025-04-20 19:45:05 +0200

    integrate recorder in device repository

--- src/domain/device_repository.h
+++ src/domain/device_repository.h
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "player.h"
+#include "recorder_i.h"
 
 namespace tone {
 
@@ -12,31 +13,40 @@ namespace tone {
 
         virtual void addPlayer(std::shared_ptr<IPlayer> player) = 0;
 
-        virtual void removePlayer(boost::uuids::uuid player_id) = 0;
+        virtual void addRecorder(std::shared_ptr<IRecorder> recorder) = 0;
 
-        virtual void startPlayer(boost::uuids::uuid player_id) const = 0;
+        virtual void removeDevice(boost::uuids::uuid device_id) = 0;
 
-        virtual void stopPlayer(boost::uuids::uuid player_id) const = 0;
+        virtual void startDevice(boost::uuids::uuid device_id) const = 0;
 
-        virtual void changePlayerFile(boost::uuids::uuid player_id, std::string file_name) const = 0;
+        virtual void stopDevice(boost::uuids::uuid device_id) const = 0;
+
+        virtual void changeDeviceFile(boost::uuids::uuid device_id, std::string file_name) const = 0;
 
         virtual std::vector<std::shared_ptr<IPlayerInfo> > getPlayersInfo() const = 0;
+
+        virtual std::vector<std::shared_ptr<IRecorderInfo> > getRecordersInfo() const = 0;
     };
 
     class DeviceRepository : public IDeviceRepository {
         std::map<boost::uuids::uuid, std::shared_ptr<IPlayer> > players{};
+        std::map<boost::uuids::uuid, std::shared_ptr<IRecorder> > recorders{};
 
     public:
         void addPlayer(std::shared_ptr<IPlayer> player) override;
 
-        void removePlayer(boost::uuids::uuid player_id) override;
+        void addRecorder(std::shared_ptr<IRecorder> recorder) override;
 
-        void startPlayer(boost::uuids::uuid player_id) const override;
+        void removeDevice(boost::uuids::uuid device_id) override;
 
-        void stopPlayer(boost::uuids::uuid player_id) const override;
+        void startDevice(boost::uuids::uuid device_id) const override;
 
-        void changePlayerFile(boost::uuids::uuid player_id, std::string file_name) const override;
+        void stopDevice(boost::uuids::uuid device_id) const override;
+
+        void changeDeviceFile(boost::uuids::uuid device_id, std::string file_name) const override;
 
         std::vector<std::shared_ptr<IPlayerInfo> > getPlayersInfo() const override;
+
+        std::vector<std::shared_ptr<IRecorderInfo> > getRecordersInfo() const override;
     };
 }
