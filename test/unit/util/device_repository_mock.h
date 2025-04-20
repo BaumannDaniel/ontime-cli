9c7bc63281368d61eb108512dfb8d41f73aa7058
daniel.baumann <baumann-dan@outlook.com>
2025-04-20 20:55:50 +0200

    add unit test for adding recorder in main input processor

--- test/unit/util/device_repository_mock.h
+++ test/unit/util/device_repository_mock.h
@@ -7,10 +7,12 @@ namespace tone::test {
     class MockDeviceRepository final : public IDeviceRepository {
     public:
         MOCK_METHOD(void, addPlayer, (std::shared_ptr<IPlayer> player), (override));
-        MOCK_METHOD(void, removePlayer, (boost::uuids::uuid player_id), (override));
-        MOCK_METHOD(void, startPlayer, (boost::uuids::uuid player_id), (const, override));
-        MOCK_METHOD(void, stopPlayer, (boost::uuids::uuid player_id), (const, override));
-        MOCK_METHOD(void, changePlayerFile, (boost::uuids::uuid player_id, std::string file_name), (const, override));
+        MOCK_METHOD(void, addRecorder, (std::shared_ptr<IRecorder> recorder), (override));
+        MOCK_METHOD(void, removeDevice, (boost::uuids::uuid player_id), (override));
+        MOCK_METHOD(void, startDevice, (boost::uuids::uuid player_id), (const, override));
+        MOCK_METHOD(void, stopDevice, (boost::uuids::uuid player_id), (const, override));
+        MOCK_METHOD(void, changeDeviceFile, (boost::uuids::uuid player_id, std::string file_name), (const, override));
         MOCK_METHOD(std::vector<std::shared_ptr<IPlayerInfo> >, getPlayersInfo, (), (const, override));
+        MOCK_METHOD(std::vector<std::shared_ptr<IRecorderInfo> >, getRecordersInfo, (), (const, override));
     };
 }
