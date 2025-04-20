7905c401435afe093a227f1519fc3dcbfafce8ff
daniel.baumann <baumann-dan@outlook.com>
2025-04-20 20:50:59 +0200

    add unit test for recorder integration in device repository

--- test/unit/domain/device_repository_test.cpp
+++ test/unit/domain/device_repository_test.cpp
@@ -1,9 +1,10 @@
-#include "device_repository.h"
-
 #include <boost/uuid/random_generator.hpp>
 #include <gtest/gtest.h>
 
+#include "device_repository.h"
+#include "recorder.h"
 #include "../util/player_mock.h"
+#include "../util/recorder_mock.h"
 
 namespace tone::test {
     class DeviceRepositoryTest : public testing::Test {
@@ -17,7 +18,15 @@ namespace tone::test {
             2,
             1
         );
+        std::shared_ptr<RecorderInfo> recorder_info_ptr = std::make_shared<RecorderInfo>(
+            boost::uuids::random_generator()(),
+            test_file_1,
+            3,
+            2,
+            1
+        );
         std::shared_ptr<MockPlayer> mock_player_ptr = std::make_shared<MockPlayer>();
+        std::shared_ptr<MockRecorder> mock_recorder_ptr = std::make_shared<MockRecorder>();
         DeviceRepository device_repository;
     };
 
@@ -46,19 +55,56 @@ namespace tone::test {
         ASSERT_EQ(player_info_ptr.get(), players_info.front().get());
     }
 
-    TEST_F(DeviceRepositoryTest, removePlayer$removesPlayerFromRepository) {
+    TEST_F(DeviceRepositoryTest, addRecorder$initalizesRecorder) {
+        auto mock_recorder = mock_recorder_ptr.get();
+        EXPECT_CALL(
+            *mock_recorder,
+            getRecorderInfo()
+        ).WillRepeatedly(testing::Return(recorder_info_ptr));
+        EXPECT_CALL(
+            *mock_recorder,
+            init()
+        ).Times(1);
+        device_repository.addRecorder(mock_recorder_ptr);
+    }
+
+    TEST_F(DeviceRepositoryTest, addRecorder$addsRecorderToRepository) {
+        auto mock_recorder = mock_recorder_ptr.get();
+        EXPECT_CALL(
+            *mock_recorder,
+            getRecorderInfo()
+        ).WillRepeatedly(testing::Return(recorder_info_ptr));
+        device_repository.addRecorder(mock_recorder_ptr);
+        auto recorders_info = device_repository.getRecordersInfo();
+        ASSERT_EQ(1, recorders_info.size());
+        ASSERT_EQ(recorder_info_ptr.get(), recorders_info.front().get());
+    }
+
+    TEST_F(DeviceRepositoryTest, removeDevice$removesPlayerFromRepository) {
         auto mock_player = mock_player_ptr.get();
         EXPECT_CALL(
             *mock_player,
             getPlayerInfo()
         ).WillRepeatedly(testing::Return(player_info_ptr));
         device_repository.addPlayer(mock_player_ptr);
-        device_repository.removePlayer(player_info_ptr->getId());
+        device_repository.removeDevice(player_info_ptr->getId());
         auto players_info = device_repository.getPlayersInfo();
         ASSERT_EQ(0, players_info.size());
     }
 
-    TEST_F(DeviceRepositoryTest, startPlayer$startsPlayer) {
+    TEST_F(DeviceRepositoryTest, removeDevice$removesRecorderFromRepository) {
+        auto mock_recorder = mock_recorder_ptr.get();
+        EXPECT_CALL(
+            *mock_recorder,
+            getRecorderInfo()
+        ).WillRepeatedly(testing::Return(recorder_info_ptr));
+        device_repository.addRecorder(mock_recorder_ptr);
+        device_repository.removeDevice(recorder_info_ptr->getId());
+        auto recorders_info = device_repository.getRecordersInfo();
+        ASSERT_EQ(0, recorders_info.size());
+    }
+
+    TEST_F(DeviceRepositoryTest, startDevice$startsPlayer) {
         auto mock_player = mock_player_ptr.get();
         EXPECT_CALL(
             *mock_player,
@@ -66,10 +112,21 @@ namespace tone::test {
         ).WillRepeatedly(testing::Return(player_info_ptr));
         device_repository.addPlayer(mock_player_ptr);
         EXPECT_CALL(*mock_player, start()).Times(1);
-        device_repository.startPlayer(player_info_ptr->getId());
+        device_repository.startDevice(player_info_ptr->getId());
     }
 
-    TEST_F(DeviceRepositoryTest, stopPlayer$stopsPlayer) {
+    TEST_F(DeviceRepositoryTest, startDevice$startsRecorder) {
+        auto mock_recorder = mock_recorder_ptr.get();
+        EXPECT_CALL(
+            *mock_recorder,
+            getRecorderInfo()
+        ).WillRepeatedly(testing::Return(recorder_info_ptr));
+        device_repository.addRecorder(mock_recorder_ptr);
+        EXPECT_CALL(*mock_recorder, start()).Times(1);
+        device_repository.startDevice(recorder_info_ptr->getId());
+    }
+
+    TEST_F(DeviceRepositoryTest, stopDevice$stopsPlayer) {
         auto mock_player = mock_player_ptr.get();
         EXPECT_CALL(
             *mock_player,
@@ -77,10 +134,21 @@ namespace tone::test {
         ).WillRepeatedly(testing::Return(player_info_ptr));
         device_repository.addPlayer(mock_player_ptr);
         EXPECT_CALL(*mock_player, stop()).Times(1);
-        device_repository.stopPlayer(player_info_ptr->getId());
+        device_repository.stopDevice(player_info_ptr->getId());
     }
 
-    TEST_F(DeviceRepositoryTest, changePlayerFile$changesFile) {
+    TEST_F(DeviceRepositoryTest, stopDevice$stopsRecorder) {
+        auto mock_player = mock_player_ptr.get();
+        EXPECT_CALL(
+            *mock_player,
+            getPlayerInfo()
+        ).WillRepeatedly(testing::Return(player_info_ptr));
+        device_repository.addPlayer(mock_player_ptr);
+        EXPECT_CALL(*mock_player, stop()).Times(1);
+        device_repository.stopDevice(player_info_ptr->getId());
+    }
+
+    TEST_F(DeviceRepositoryTest, changeDeviceFile$changesPlayerFile) {
         auto mock_player = mock_player_ptr.get();
         EXPECT_CALL(
             *mock_player,
@@ -88,6 +156,6 @@ namespace tone::test {
         ).WillRepeatedly(testing::Return(player_info_ptr));
         device_repository.addPlayer(mock_player_ptr);
         EXPECT_CALL(*mock_player, changeFile(test_file_2)).Times(1);
-        device_repository.changePlayerFile(player_info_ptr->getId(), test_file_2);
+        device_repository.changeDeviceFile(player_info_ptr->getId(), test_file_2);
     }
 }
